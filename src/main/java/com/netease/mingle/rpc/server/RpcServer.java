5183c7c28c0e7281f0595dd21af2a0629d31e794
mymonkey110 <mymonkey110@163.com>
2016-11-27 23:45:34 +0800

    make it work

--- src/main/java/com/netease/mingle/rpc/server/RpcServer.java
+++ src/main/java/com/netease/mingle/rpc/server/RpcServer.java
@@ -3,20 +3,25 @@ package com.netease.mingle.rpc.server;
 import com.netease.mingle.rpc.shared.HttpChannelInitializer;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.nio.NioEventLoop;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpServerCodec;
 
 /**
  * Created by Michael Jiang on 2016/11/27.
  */
 public class RpcServer {
-    private ServiceRegister serviceRegister;
     private int servicePort;
 
-    public RpcServer(ServiceRegister serviceRegister, int servicePort) {
-        this.serviceRegister = serviceRegister;
+    public RpcServer(int servicePort) {
         this.servicePort = servicePort;
     }
 
@@ -25,8 +30,17 @@ public class RpcServer {
         ServerBootstrap bootstrap = new ServerBootstrap();
         bootstrap.group(group).channel(NioServerSocketChannel.class)
                 .localAddress(servicePort)
-                .childHandler(new HttpChannelInitializer(false))
-                .childHandler(new ServiceHandler())
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast("decoder", new HttpRequestDecoder());
+                        pipeline.addLast("codec", new HttpServerCodec());
+                        pipeline.addLast("aggregator", new HttpObjectAggregator(64 * 1024));
+                        pipeline.addLast(new ServiceHandler());
+                        pipeline.addLast("encoder", new HttpResponseEncoder());
+                    }
+                })
                 .option(ChannelOption.SO_BACKLOG, 128)
                 .childOption(ChannelOption.SO_KEEPALIVE, true);
 
@@ -39,6 +53,10 @@ public class RpcServer {
         } finally {
             group.shutdownGracefully();
         }
+    }
 
+    public static void main(String[] args) {
+        RpcServer rpcServer = new RpcServer(1616);
+        rpcServer.start();
     }
 }
--- src/main/java/com/netease/mingle/rpc/server/ServiceHandler.java
+++ src/main/java/com/netease/mingle/rpc/server/ServiceHandler.java
@@ -1,6 +1,7 @@
 package com.netease.mingle.rpc.server;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.*;
@@ -14,11 +15,30 @@ import org.slf4j.LoggerFactory;
 public class ServiceHandler extends ChannelInboundHandlerAdapter {
     private static Logger logger = LoggerFactory.getLogger(ServiceHandler.class);
 
+    private HttpRequest httpRequest;
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        DefaultFullHttpRequest request = (DefaultFullHttpRequest) msg;
-        DefaultFullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,HttpResponseStatus.OK,request.content());
-        ctx.writeAndFlush(response);
+        if (msg instanceof HttpRequest) {
+            httpRequest = (HttpRequest) msg;
+            logger.info("request uri:{}.",httpRequest.uri());
+        }
+
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            logger.info(buf.toString());
+            buf.release();
+
+            String res = "I'm ok";
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,HttpResponseStatus.OK, Unpooled.wrappedBuffer(res.getBytes("UTF-8")));
+            response.headers().set("Content-Type","text/plain");
+            response.headers().set("Content-Length",response.content().readableBytes());
+            if (HttpUtil.isKeepAlive(httpRequest)) {
+                response.headers().set("Connection", HttpHeaderValues.KEEP_ALIVE);
+            }
+            ctx.writeAndFlush(response);
+        }
     }
 
     @Override
