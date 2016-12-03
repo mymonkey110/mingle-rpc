5d2352d3fa3df1c5b6bdd0a1d2e370d88a774e4d
jiangwenkang <hzjiangwenkang@corp.netease.com>
2016-12-04 00:17:12 +0800

    fix client

--- src/main/java/com/netease/mingle/rpc/client/RpcClient.java
+++ src/main/java/com/netease/mingle/rpc/client/RpcClient.java
@@ -1,35 +1,100 @@
 package com.netease.mingle.rpc.client;
 
+import com.netease.mingle.rpc.shared.InnerLoggerFactory;
+import com.netease.mingle.rpc.shared.MethodInvocation;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.oio.OioSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 /**
  * Rpc Client
  * Created by Michael Jiang on 2016/12/2.
  */
 public class RpcClient {
-    private Map<Class,ServiceAddress> serviceAddressMap = new ConcurrentHashMap<>(8);
-    private Map<ServiceAddress,Boolean> serviceAddressInitializeMap = new ConcurrentHashMap<>(8);
+    private Map<Class, ServiceAddress> serviceAddressMap = new ConcurrentHashMap<>(8);
+    private Map<ServiceAddress, Boolean> serviceAddressInitializeMap = new ConcurrentHashMap<>(8);
+    private static Logger logger = InnerLoggerFactory.getLogger(RpcClient.class.toString());
 
-    public void proxy(Class serviceInterface,String serviceAddress) {
-
+    @SuppressWarnings("unchecked")
+    public synchronized <T> T proxy(Class<T> serviceInterface, String address) {
+        if (!serviceAddressMap.containsKey(serviceInterface)) {
+            ServiceAddress serviceAddress = ServiceAddress.parse(address);
+            serviceAddressMap.put(serviceInterface, serviceAddress);
+            initializeServiceAddress(serviceAddress);
+            return (T) new RpcProxy(serviceInterface).getProxyObject();
+        } else {
+            throw new IllegalArgumentException("already proxy service:" + serviceInterface.getName());
+        }
     }
 
-    private static Bootstrap bootstrap=new Bootstrap();
+    private static Bootstrap bootstrap = new Bootstrap();
 
     static {
         bootstrap.group(new OioEventLoopGroup())
                 .channel(OioSocketChannel.class)
-                .handler(new ClientHandler());
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new ObjectDecoder(ClassResolvers.cacheDisabled(this.getClass().getClassLoader())));
+                        pipeline.addLast(new ClientHandler());
+                        pipeline.addLast(new ObjectEncoder());
+                    }
+                });
     }
 
-    private void initializeServiceAddress(ServiceAddress serviceAddress) {
-        synchronized (serviceAddress) {
+    private synchronized void initializeServiceAddress(final ServiceAddress serviceAddress) {
+        if (!serviceAddressInitializeMap.get(serviceAddress)) {
+            ChannelFuture channelFuture = bootstrap.remoteAddress(serviceAddress.toInetSocketAddress()).connect();
+            channelFuture.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (future.isSuccess()) {
+                        logger.info("connected to service host:" + serviceAddress);
+                        serviceAddressInitializeMap.put(serviceAddress, true);
+                    } else {
+                        logger.warning("can't connected to service host:" + serviceAddress);
+                    }
+                }
+            });
+        }
+    }
 
+    class RpcProxy<T> implements InvocationHandler {
+        private Class<T> service;
+
+        RpcProxy(Class<T> service) {
+            if (!service.isInterface()) {
+                throw new IllegalArgumentException("mingle can only proxy interface now");
+            }
+            this.service = service;
+        }
+
+        @SuppressWarnings("unchecked")
+        T getProxyObject() {
+            return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{service}, this);
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            MethodInvocation methodInvocation = MethodInvocation.from(method);
+
+            return null;
         }
     }
 
--- src/main/java/com/netease/mingle/rpc/client/ServiceAddress.java
+++ src/main/java/com/netease/mingle/rpc/client/ServiceAddress.java
@@ -12,10 +12,21 @@ public class ServiceAddress {
 
     private ServiceAddress(String ip, int port) {
         if (!isValidPort(port)) {
-            throw new IllegalArgumentException("port:"+port+" is illegal");
+            throw new IllegalArgumentException("port:" + port + " is illegal");
         }
-        this.ip=ip;
+        this.ip = ip;
         this.port = port;
+    }
+
+    public static ServiceAddress parse(String address) {
+        String[] ipAndPort = address.split(":");
+        if (ipAndPort.length == 2) {
+            return new ServiceAddress(ipAndPort[0], Integer.parseInt(ipAndPort[1]));
+        } else if (ipAndPort.length == 1) {
+            return new ServiceAddress(ipAndPort[0], 80);
+        } else {
+            throw new IllegalArgumentException("address:" + address + " is illegal");
+        }
     }
 
     private static boolean isValidPort(int port) {
@@ -23,6 +34,14 @@ public class ServiceAddress {
     }
 
     public InetSocketAddress toInetSocketAddress() {
-        return new InetSocketAddress(ip,port);
+        return new InetSocketAddress(ip, port);
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceAddress{" +
+                "ip='" + ip + '\'' +
+                ", port=" + port +
+                '}';
     }
 }
